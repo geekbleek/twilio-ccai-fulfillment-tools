df16dfc722ff8d9beecfc956bb2338c408df99e7
James King <ripixel@gmail.com>
2020-06-15 20:16:25 +0100

    fix: Make templates a required parameter for dynamic context panel creators

--- src/DynamicContextPanel/index.ts
+++ src/DynamicContextPanel/index.ts
@@ -21,7 +21,7 @@ interface IContext<T extends SYNC_OBJECT_TYPES, U extends TDatas> {
     name: 'sync-attributes';
     lifespan: number;
     parameters: {
-        sync_attributes?: {
+        sync_attributes: {
             sync_object_type: T;
             data: U;
         };
@@ -39,52 +39,46 @@ export type TMapContext = IContext<SYNC_OBJECT_TYPES.MAP, TMapData>;
 const LIFESPAN = 99;
 
 export const createDynamicContextPanelDocument = (
-    templates?: TDocumentData,
+    templates: TDocumentData,
     routingAttributes?: IStringKeyMap<unknown>
 ): TDocumentContext => ({
     name: 'sync-attributes',
     lifespan: LIFESPAN,
     parameters: {
-        sync_attributes: templates
-            ? {
-                  sync_object_type: SYNC_OBJECT_TYPES.DOCUMENT,
-                  data: templates,
-              }
-            : undefined,
+        sync_attributes: {
+            sync_object_type: SYNC_OBJECT_TYPES.DOCUMENT,
+            data: templates,
+        },
         routing_attributes: routingAttributes,
     },
 });
 
 export const createDynamicContextPanelList = (
-    templates?: TListData,
+    templates: TListData,
     routingAttributes?: IStringKeyMap<unknown>
 ): TListContext => ({
     name: 'sync-attributes',
     lifespan: LIFESPAN,
     parameters: {
-        sync_attributes: templates
-            ? {
-                  sync_object_type: SYNC_OBJECT_TYPES.LIST,
-                  data: templates,
-              }
-            : undefined,
+        sync_attributes: {
+            sync_object_type: SYNC_OBJECT_TYPES.LIST,
+            data: templates,
+        },
         routing_attributes: routingAttributes,
     },
 });
 
 export const createDynamicContextPanelMap = (
-    templates?: TMapData,
+    templates: TMapData,
     routingAttributes?: IStringKeyMap<unknown>
 ): TMapContext => ({
     name: 'sync-attributes',
     lifespan: LIFESPAN,
     parameters: {
-        sync_attributes: templates
-            ? {
-                  sync_object_type: SYNC_OBJECT_TYPES.MAP,
-                  data: templates,
-              }
-            : undefined,
+        sync_attributes: {
+            sync_object_type: SYNC_OBJECT_TYPES.MAP,
+            data: templates,
+        },
         routing_attributes: routingAttributes,
     },
 });
